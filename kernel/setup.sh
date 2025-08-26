1cbd68e4133dae2857ee8f7c70c798568e8ab11a
sakana164 <76257039+sakana164@users.noreply.github.com>
2025-08-26 15:52:59 +0800

    workflows: Add build manger only 5ec1cff/KernelSU@a3e8343 5ec1cff/KernelSU@d3fc92a upstream Enable non-gki Branch Sync upstream workflows changes

--- kernel/setup.sh
+++ kernel/setup.sh
@@ -43,7 +43,7 @@ setup_kernelsu() {
     cd "$GKI_ROOT/KernelSU"
     git stash && echo "[-] Stashed current changes."
     if [ "$(git status | grep -Po 'v\d+(\.\d+)*' | head -n1)" ]; then
-        git checkout main && echo "[-] Switched to main branch."
+        git checkout non-gki && echo "[-] Switched to non-gki branch."
     fi
     git pull && echo "[+] Repository updated."
     if [ -z "${1-}" ]; then
