c1bd9cf16a9af63ed3a274cafb2e33b86fce923a
sakana164 <76257039+sakana164@users.noreply.github.com>
2025-08-26 15:50:38 +0800

    Reapply "Reset seccomp filter count when escaping to root (#2708)"
    This reapply commit a41bcf791d815cf0e56b56f9001b8bc3fc31935f

--- kernel/core_hook.c
+++ kernel/core_hook.c
@@ -129,7 +129,10 @@ static void disable_seccomp()
 #ifdef CONFIG_SECCOMP
 	current->seccomp.mode = 0;
 	current->seccomp.filter = NULL;
+// https://elixir.bootlin.com/linux/v5.9-rc1/source/include/linux/seccomp.h
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 9, 0)
 	atomic_set(&current->seccomp.filter_count, 0);
+#endif
 #else
 #endif
 }
