f7e7049cf81efc451686220983a5f34d1f5cd6fe
sakana164 <76257039+sakana164@users.noreply.github.com>
2025-11-17 19:08:25 +0800

    kernel: restore support code for nongki
    Signed-off-by: sakana164 <76257039+sakana164@users.noreply.github.com>

--- kernel/allowlist.c
+++ kernel/allowlist.c
@@ -10,7 +10,14 @@
 #include <linux/slab.h>
 #include <linux/types.h>
 #include <linux/version.h>
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 10, 0)
+#include <linux/sched/task.h>
+#else
+#include <linux/sched.h>
+#endif
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 14, 0)
 #include <linux/compiler_types.h>
+#endif
 
 #include "klog.h" // IWYU pragma: keep
 #include "ksud.h"
