7066252a7a8d406780cbf7020370c437732a3ded
sakana164 <76257039+sakana164@users.noreply.github.com>
2026-01-17 00:21:44 +0800

    kernel: ksud: provide newfstat_ret handler
    Signed-off-by: sakana164 <76257039+sakana164@users.noreply.github.com>

--- kernel/ksud.c
+++ kernel/ksud.c
@@ -475,6 +475,83 @@ int ksu_handle_sys_read(unsigned int fd, char __user **buf_ptr,
     return 0;
 }
 
+#define STAT_NATIVE 0
+#define STAT_STAT64 1
+
+static noinline void ksu_common_newfstat_ret(unsigned long fd_long, void **statbuf_ptr, const int type)
+{
+    if (!is_init(current_cred()))
+        return;
+
+    struct file *file = fget(fd_long);
+    if (!file)
+        return;
+
+    if (!is_init_rc(file)) {
+        fput(file);
+        return;
+    }
+    fput(file);
+
+    pr_info("%s: stat init.rc \n", __func__);
+
+    uintptr_t statbuf_ptr_local = (uintptr_t)*(void **)statbuf_ptr;
+    void __user *statbuf = (void __user *)statbuf_ptr_local;
+    if (!statbuf)
+        return;
+
+    void __user *st_size_ptr;
+    long size, new_size;
+    size_t len;
+
+    st_size_ptr = statbuf + offsetof(struct stat, st_size);
+    len = sizeof(long);
+
+#if defined(__ARCH_WANT_STAT64) || defined(__ARCH_WANT_COMPAT_STAT64)
+    if (type) {
+        st_size_ptr = statbuf + offsetof(struct stat64, st_size);
+        len = sizeof(long long);
+    }
+#endif
+
+    if (copy_from_user(&size, st_size_ptr, len)) {
+        pr_info("%s: read statbuf 0x%lx failed \n", __func__, (unsigned long)st_size_ptr);
+        return;
+    }
+
+    new_size = size + ksu_rc_len;
+    pr_info("%s: adding ksu_rc_len: %ld -> %ld \n", __func__, size, new_size);
+        
+    if (!copy_to_user(st_size_ptr, &new_size, len))
+        pr_info("%s: added ksu_rc_len \n", __func__);
+    else
+        pr_info("%s: add ksu_rc_len failed: statbuf 0x%lx \n", __func__, (unsigned long)st_size_ptr);
+    
+    return;
+}
+
+void ksu_handle_newfstat_ret(unsigned int *fd, struct stat __user **statbuf_ptr)
+{
+    unsigned long fd_long = (unsigned long)*fd;
+
+    if (likely(!ksu_init_rc_hook))
+        return;
+
+    ksu_common_newfstat_ret(fd_long, (void **)statbuf_ptr, STAT_NATIVE);
+}
+
+#if defined(__ARCH_WANT_STAT64) || defined(__ARCH_WANT_COMPAT_STAT64)
+void ksu_handle_fstat64_ret(unsigned long *fd, struct stat64 __user **statbuf_ptr)
+{
+    unsigned long fd_long = (unsigned long)*fd;
+
+    if (likely(!ksu_init_rc_hook))
+        return;
+
+    ksu_common_newfstat_ret(fd_long, (void **)statbuf_ptr, STAT_STAT64);
+}
+#endif
+
 static unsigned int volumedown_pressed_count = 0;
 
 static bool is_volumedown_enough(unsigned int count)
