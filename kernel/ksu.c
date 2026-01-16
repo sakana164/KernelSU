8d3ebe92b0fad3e04f12dd5edd8f0d1fc8c9bf10
sakana164 <76257039+sakana164@users.noreply.github.com>
2026-01-17 00:19:52 +0800

    kernel: Back manual hooks and KPROBES guard for nongki
    Add ksu_init_rc_hook
    compart ksu_handle_sys_read
    
    Revert "kernel: remove unused"
    This reverts commit e9b232f316628d94ade11c61e204a2e6fe5047da.
    
    Revert "kernel: remove unused guard"
    This reverts commit cf42e0f96a7fa26b5e2804885c96617c570ff3e7.
    
    Signed-off-by: sakana164 <76257039+sakana164@users.noreply.github.com>

--- kernel/ksu.c
+++ kernel/ksu.c
@@ -14,6 +14,20 @@
 #include "ksu.h"
 #include "file_wrapper.h"
 
+extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
+                    void *argv, void *envp, int *flags);
+
+extern int ksu_handle_execveat_ksud(int *fd, struct filename **filename_ptr,
+                    void *argv, void *envp, int *flags);
+
+int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
+            void *envp, int *flags)
+{
+    ksu_handle_execveat_ksud(fd, filename_ptr, argv, envp, flags);
+    return ksu_handle_execveat_sucompat(fd, filename_ptr, argv, envp,
+                        flags);
+}
+
 // workaround for A12-5.10 kernel
 // Some third-party kernel (e.g. linegaeOS) uses wrong toolchain, which supports
 // CC_HAVE_STACKPROTECTOR_SYSREG while gki's toolchain doesn't.
@@ -80,7 +94,11 @@ int __init kernelsu_init(void)
 
     ksu_throne_tracker_init();
 
+#ifdef CONFIG_KPROBES
     ksu_ksud_init();
+#else
+    pr_alert("KPROBES is disabled, KernelSU may not work, please check manual hooks.");
+#endif
 
     ksu_file_wrapper_init();
 
@@ -101,7 +119,9 @@ void kernelsu_exit(void)
 
     ksu_observer_exit();
 
+#ifdef CONFIG_KPROBES
     ksu_ksud_exit();
+#endif
 
     ksu_syscall_hook_manager_exit();
 
