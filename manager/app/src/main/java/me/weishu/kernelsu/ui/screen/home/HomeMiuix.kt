b54fdb5f128812c9128b1e589eba234925ba53e7
KOWX712 <leecc0503@gmail.com>
2026-03-12 20:22:47 +0800

    manager: QOL
    - default to material ui
    - remove warning card
    - U-LEGACY and LEGACY working mode
    - gray out magica

--- manager/app/src/main/java/me/weishu/kernelsu/ui/screen/home/HomeMiuix.kt
+++ manager/app/src/main/java/me/weishu/kernelsu/ui/screen/home/HomeMiuix.kt
@@ -126,9 +126,6 @@ fun HomePagerMiuix(
                     } else if (state.showKernelPrBuildWarning) {
                         WarningCard(stringResource(id = R.string.home_pr_kernel_warning))
                     }
-                    if (state.showGkiWarning) {
-                        WarningCard(stringResource(id = R.string.home_gki_warning))
-                    }
                     if (state.showRequireKernelWarning) {
                         WarningCard(
                             stringResource(id = R.string.require_kernel_version)
